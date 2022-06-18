1c82bd4de7412e5baefbda701344c9d0a115a7c7
JulianaTagami <jutagamiss@gmail.com>
2022-06-18 13:11:17 +0100

    section banner

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+$('#collapse-navbar').on('show.bs.collapse', function(){
+    $('.banner').css('transform', 'translate(-50%, 10%)');
+
+    } 
+    
+)
+
+$('#collapse-navbar').on('hide.bs.collapse', function(){
+    $('.banner').css('transform', 'translate(-50%, -50%)');
+
+    } 
+    
+)
