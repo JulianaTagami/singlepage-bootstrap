cd083a732da03887116dcdb30b845fb2e6f6ff88
JulianaTagami <jutagamiss@gmail.com>
2022-06-19 19:33:21 +0100

    option translate navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 $('#collapse-navbar').on('show.bs.collapse', function(){
-    $('.banner').css('transform', 'translate(-50%, 10%)');
+    $('#banner').css('transform', 'translate(-50%, 10%)');
 
     } 
     
 )
 
 $('#collapse-navbar').on('hide.bs.collapse', function(){
-    $('.banner').css('transform', 'translate(-50%, -50%)');
+    $('#banner').css('transform', 'translate(-50%, -50%)');
 
     } 
     
